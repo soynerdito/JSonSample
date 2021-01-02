4b6c068ff4ddd3a44e028e44f1af67d112e4cde7
Soynerdito <soynerdito@gmail.com>
2021-01-02 15:13:16 -0400

    initial commit

---
+++ JSonSample/JsonFileStore.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace JSonSample
+{
+    public class ConfigFileStruct
+    {
+        public Dictionary<string, string> Keys { get; set; } = new Dictionary<string, string>();
+    }
+    public class JsonFileStore : SSH.Stores.IStore
+    {
+        private ConfigFileStruct _settings = new ConfigFileStruct();
+
+        protected string FileName => Path.Combine(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".possh"), "hosts.json");
+        public IDictionary<string, string> GetKeys()
+        {
+            LoadFromDisk();
+            return _settings.Keys;
+        }
+
+        public bool SetKey(string host, string fingerprint)
+        {
+            _settings.Keys.Remove(host);
+            _settings.Keys.Add(host, fingerprint);
+            WriteToDisk();
+            return true;
+        }
+
+        public void LoadFromDisk()
+        {
+            var jsonString = File.ReadAllText(FileName);
+            _settings = JsonSerializer.Deserialize<ConfigFileStruct>(jsonString);
+        }
+      
+        private void WriteToDisk()
+        {
+            var jsonString = JsonSerializer
+                .Serialize<ConfigFileStruct>(_settings, new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+            Directory.CreateDirectory(Path.GetDirectoryName(FileName));
+            File.WriteAllText(FileName, jsonString);
+        }
+    }
+}
---
+++ JSonSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JSonSample
+{
+    class Program
+    {
+        static void PrintKeys(List<KeyValuePair<string, string>> keys)
+        {
+            Console.WriteLine("****");
+            Console.WriteLine("Print file");
+            keys.ForEach(x => Console.WriteLine($"key: {x.Key} value: {x.Value}"));
+            Console.WriteLine("****");
+        }
+        static void Main(string[] args)
+        {
+            var fileStore = new JsonFileStore();
+            fileStore.SetKey("molleja", "caliente");
+            PrintKeys(fileStore.GetKeys().ToList());
+            fileStore.SetKey("molleja2", "frias");
+            PrintKeys(fileStore.GetKeys().ToList());            
+            fileStore.SetKey("molleja", "caliente");
+            PrintKeys(fileStore.GetKeys().ToList());
+        }
+
+    }
+}
